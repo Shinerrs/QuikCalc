c3bf2e69e216319fc5610faed4e55691effc7675
Mark Shine <shinerrs@Marks-Mac-Pro.local>
2015-02-26 12:12:25 +0000

    GUI Improved with Equal Width

--- QuikCalc/ViewController.swift
+++ QuikCalc/ViewController.swift
@@ -9,18 +9,18 @@
 import UIKit
 
 /*
-    1. User presses digit - number appended to string (up to 10 digits)
-    2. User presses operator:
-        - number converted to int
-        - operator logged
-        - gets ready for next number
-    3. User presses =
-        - currentTotal displayed (including any calculated value)
-    4. User presses C
-        - All clear
-    5. User presses D:
-        - last digit of displayLabel deleted
-        - if 0 or 1 digits, back to 0
+1. User presses digit - number appended to string (up to 10 digits)
+2. User presses operator:
+- number converted to int
+- operator logged
+- gets ready for next number
+3. User presses =
+- currentTotal displayed (including any calculated value)
+4. User presses C
+- All clear
+5. User presses D:
+- last digit of displayLabel deleted
+- if 0 or 1 digits, back to 0
 */
 
 
@@ -38,18 +38,18 @@ class ViewController: UIViewController {
         
         displayLabel.text = "\(currentTotal)"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     /*
-        If 0 display, start a new one, otherwise append
+    If 0 display, start a new one, otherwise append
     
-        If operator set, start new number
+    If operator set, start new number
     
-        Is this it?
+    Is this it?
     */
     @IBAction func pressNumber(sender: AnyObject) {
         
@@ -64,33 +64,33 @@ class ViewController: UIViewController {
     }
     
     /*
-        If no currentValue, copy in displayLabel. Set operator
-        If currentValue, set operator and recalculate
-        Is this it?
+    If no currentValue, copy in displayLabel. Set operator
+    If currentValue, set operator and recalculate
+    Is this it?
     */
     @IBAction func pressOperator(sender: AnyObject) {
         let o:String! = (sender as UIButton).titleLabel?.text
         
         switch(o){
-            case "+":
-                // add operation
-                oper = "+"
-            case "-":
-                // subtract operation
-                oper = "-"
-            case "x":
-                // multiply operation
-                oper = "x"
-            case "/":
-                // divide operation
-                oper = "/"
+        case "+":
+            // add operation
+            oper = "+"
+        case "-":
+            // subtract operation
+            oper = "-"
+        case "x":
+            // multiply operation
+            oper = "x"
+        case "/":
+            // divide operation
+            oper = "/"
         default:
             oper = nil
         }
     }
-
+    
     /*
-        Perform calculation and display result
+    Perform calculation and display result
     */
     @IBAction func equalsOp (sender: AnyObject) {
         
@@ -98,7 +98,7 @@ class ViewController: UIViewController {
     }
     
     /*
-        Reset all values
+    Reset all values
     */
     @IBAction func clearOp (sender: AnyObject) {
         currentTotal = 0
@@ -108,8 +108,8 @@ class ViewController: UIViewController {
     }
     
     /*
-        if displayLabel has more than one digit, append
-        else reset to 0
+    if displayLabel has more than one digit, append
+    else reset to 0
     */
     @IBAction func deleteOp (sender: AnyObject) {
         var display:String! = displayLabel.text
@@ -125,4 +125,3 @@ class ViewController: UIViewController {
     }
     
 }
-
